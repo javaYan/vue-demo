574614c33b2c3c7d67c037309b63e1377bc000a8
javaYan <834595596@qq.com>
2018-01-16 22:08:01 +0800

    vue-basic:     verfication code验证码练习

--- vue-basic/web/resources/js/vue-components.js
+++ vue-basic/web/resources/js/vue-components.js
@@ -1,32 +1,38 @@
+//数字和字母的验证码组件
 var verificationCodeComponent = Vue.extend({
-    template: '<div id="verificationCode">' +
-                    '<input type="text" id="inputCode" @blur="checkCode()" placeholder="请输入验证码">' +
-                    '<input type="button" id="randomCode" @click="createCode()">' +
-                    '<span id="checkResult" style="color:red"></span>' +
-              '</div>',
-    data: function () {
-        return {checkResult:true}
+    template:'<div class="form-group" v-cloak>' +
+    '<input type="text" id="vcInputCode" class="form-control" placeholder="输入右侧图片验证码" @blur="checkInputCode()">' +
+    '<input type="button" id="vcRandomCode" class="btn btn-secondary" style="text-shadow: black 1px 1px 1px;" @click="createRandomCode()"/>' +
+    '<i v-if="vcCheckResult == true" class="fa fa-check desc-success"></i><i v-if="vcCheckResult == false" class="fa fa-remove desc-danger"></i>' +
+    '</div>',
+    data: function() {
+        return {
+            vcCheckResult:'N'
+        }
     },
-    mounted: function () {
-        this.createCode();
+    mounted:function(){
+        this.createRandomCode();
     },
-    methods: {
-        createCode: function () {
-            var random = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+    methods:{
+        createRandomCode: function () {
+            this.vcCheckResult = 'N';
+            var sourceCode = new Array(2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z');
+            var showCode = '';
             var code = '';
-            for (var i = 0; i < 4; i++) {
-                var index = Math.floor(Math.random() * 36);
-                code += random[index];
+            for(var i = 0; i < 4; i++) {
+                var index = Math.floor(Math.random()*32);
+                showCode += sourceCode[index] + ' ';
+                code += sourceCode[index];
             }
-            $('#randomCode').val(code);
-            $('#checkResult').text('');
-            $('#inputCode').focus();
+            $('#vcRandomCode').val(showCode);
+            $('#vcRandomCode').attr("data-val", code);
+            $('#vcInputCode').focus();
         },
-        checkCode: function () {
-            if ($('#inputCode').val() == $('#randomCode').val()) {
-                $('#checkResult').text("验证成功");
+        checkInputCode: function () {
+            if ($('#vcRandomCode').attr("data-val").toLowerCase() == $('#vcInputCode').val().toLowerCase()) {
+                this.vcCheckResult = true;
             } else {
-                $('#checkResult').text("验证码不匹配");
+                this.vcCheckResult = false;
             }
         }
     }
